62d87ff71ec5f3f685635db12d05ef39aa115bcb
Namila Bandara <namila007@gmail.com>
2019-02-27 18:42:57 +0530

    REST for user
    Signed-off-by: Namila Bandara <namila007@gmail.com>

---
+++ src/main/java/hello/Controller/UserController.java
@@ -0,0 +1,47 @@
+package hello.Controller;
+
+import hello.Model.User;
+import hello.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController
+{
+
+
+	@Autowired
+	private UserService userService;
+
+	//GET a user from ID
+	@GetMapping("/{id}")
+	public ResponseEntity<?> showbyId(@PathVariable(value = "id") int id )
+	{
+		return new ResponseEntity<User>(userService.getUser( id ), HttpStatus.OK) ;
+	}
+
+	//GET get all users list
+	@GetMapping()
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<List<User>>(userService.getAll(),HttpStatus.OK);
+    }
+
+    //POST create a new user
+	@PostMapping()
+    public ResponseEntity<?> addUser( @RequestBody User user) {
+
+	    return new ResponseEntity<User>(userService.addUser(user), HttpStatus.CREATED);
+    }
+
+    //Delete User
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletUserbyId(@PathVariable(value="id") int id) {
+	    return new ResponseEntity<User>(userService.deletUserbyId(id), HttpStatus.OK);
+    }
+
+}
--- src/main/java/hello/Service/UserService.java
+++ src/main/java/hello/Service/UserService.java
@@ -1,5 +1,42 @@
 package hello.Service;
 
+import hello.Model.User;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
 public class UserService
 {
+	private static List<User> userList;
+
+
+	public UserService()
+	{
+		userList = new ArrayList<>();
+
+	}
+
+	public User addUser( User user )
+	{
+        user.setId(userList.size());
+		userList.add( user );
+		return user;
+	}
+
+	public User getUser( int id )
+	{
+		return userList.get( id );
+	}
+
+	public List<User> getAll () {
+	    return userList;
+    }
+
+    public User deletUserbyId(int id) {
+	    return userList.remove(id);
+    }
 }
