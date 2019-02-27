6795804a4b017ff984590e244a07d28ee8e05853
Namila Bandara <namila007@gmail.com>
2019-02-27 17:12:17 +0530

    update
    Signed-off-by: Namila Bandara <namila007@gmail.com>

---
+++ src/main/java/hello/Model/User.java
@@ -0,0 +1,16 @@
+package hello.Model;
+
+import lombok.Data;
+
+@Data
+public class User
+{
+	private String name;
+	private int id;
+
+	public User( String name, int id )
+	{
+		this.name = name;
+		this.id = id;
+	}
+}
---
+++ src/main/java/hello/Service/UserService.java
@@ -0,0 +1,5 @@
+package hello.Service;
+
+public class UserService
+{
+}
